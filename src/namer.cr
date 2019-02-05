4589c6580e7b5836ed7c89b5bd0f78df5f66ce97
Will Lewis <will@wflewis.com>
2019-02-04 23:29:04 -0500

    Add kebab option and help display

--- src/namer.cr
+++ src/namer.cr
@@ -6,18 +6,43 @@ module Namer
 
   extend self
 
-  def generate
+  def generate(glue : String = " ")
     noun = NOUNS.shuffle.first
     adj = ADJECTIVES.shuffle.first
 
-    [adj, noun].join(" ")
+    [adj, noun].join(glue)
   end
 end
 
-unless (ARGV & %w(-c --continuous)).empty?
-  loop do
+def has_arg(args : Array(String))
+  !(ARGV & args).empty?
+end
+
+if has_arg(%w(-v --version))
+  puts "namer v#{Namer::VERSION}"
+  exit 0
+end
+
+if has_arg(%w(-h --help))
+  puts <<-USAGE
+    usage: namer [ARGS]
+    args:
+      --version     show version
+      --help        show help
+      --kebab       use kebab case
+      --continuous  continuous print forever
+  USAGE
+  exit 0
+end
+
+continuous = has_arg(%w(-c --continuous))
+
+loop do
+  if has_arg(%w(-k --kebab))
+    Namer.generate("-").puts
+  else
     Namer.generate.puts
   end
-else
-  Namer.generate.puts
+
+  break if !continuous
 end
